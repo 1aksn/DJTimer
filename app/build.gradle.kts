4b80414d794ed06e11280594ad84f04fe29e06c3
kaito-furukawa <kaito.furukawa@play.jp>
2026-04-06 01:39:54 +0900

    versionName,versionCode更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.djtimer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 //        testInstrumentationRunner = "com.google.dagger.hilt.android.testing.HiltTestRunner"
